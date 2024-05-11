3893b9bf8803342730881d6d382fa433ead0d244
PiotrDrechsler <pdrech@gmail.com>
2024-05-11 16:10:40 +0200

    create main view
    1.add SafeAreaView  component
    2.add ScrollView component
    3,add View with styling
    4.import images
    5.add images
    6.add text

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,51 @@
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../constants";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Index file here!</Text>
-      <Link href="/home" className="font-normal color-blue-500">
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="h-full">
+      <ScrollView
+        className="bg-primary"
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[298px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">IMG-FORGE</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with IMG-FOrRGE
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
