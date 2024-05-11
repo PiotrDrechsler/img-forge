9715948eb162b94ef15dbcf4faf506b38c730ca3
PiotrDrechsler <pdrech@gmail.com>
2024-05-11 12:51:33 +0200

    fix/font problem
    when font is no bold <TEXT> works wrong - it don't show all what you type into it
    how to fix it?
    add - bold to class, try other types of font-weight

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "@/components/Themed";
 export default function TabTwoScreen() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl text-center p-4">Tab TWO!</Text>
+      <Text className="text-3xl text-center font-bold p-4">Tab TWO!</Text>
       <View />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
