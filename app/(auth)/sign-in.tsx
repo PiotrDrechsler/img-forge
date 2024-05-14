325e3ef3c20b5c88d30f6860a82a92959278c19b
PiotrDrechsler <pdrech@gmail.com>
2024-05-14 17:33:00 +0200

    create/SignIn
    1.import and add SafeAreaView with styles
    2.import and add ScrollView add styles
    3.import images
    4.add text with styles

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,29 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { Image, ScrollView, View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../../constants";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="h-full" style={{ backgroundColor: "#161622" }}>
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[34px]"
+          />
+          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
+            Log in to Img-Forge
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
